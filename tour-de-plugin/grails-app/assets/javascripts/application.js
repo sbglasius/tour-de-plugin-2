1179d4a0d7899136d016f957af09b118652db066
Søren Berg Glasius <soeren@glasius.dk>
2016-01-14 13:05:55 +0100

    Addin css/js

--- tour-de-plugin/grails-app/assets/javascripts/application.js
+++ tour-de-plugin/grails-app/assets/javascripts/application.js
@@ -6,7 +6,7 @@
 // to create separate JavaScript files as needed.
 //
 //= require jquery-2.1.3.js
-//= require_tree .
+//= require bootstrap-all.js
 //= require_self
 
 if (typeof jQuery !== 'undefined') {
