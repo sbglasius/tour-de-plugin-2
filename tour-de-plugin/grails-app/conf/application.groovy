c4def9a5424101fd34a80d7e5efc54a74a3bf3d8
Søren Berg Glasius <soeren@glasius.dk>
2016-01-16 01:40:58 +0530

    Greenmail

--- tour-de-plugin/grails-app/conf/application.groovy
+++ tour-de-plugin/grails-app/conf/application.groovy
@@ -1,13 +1,29 @@
 grails.config.locations = [ "file:${userHome}/.grails/tour-de-plugin-config.groovy"]
-grails {
-    mail {
-        host = "smtp.gmail.com"
-        port = 465
-        username = "youracount@gmail.com"
-        password = "yourpassword"
-        props = ["mail.smtp.auth"                  : "true",
-                 "mail.smtp.socketFactory.port"    : "465",
-                 "mail.smtp.socketFactory.class"   : "javax.net.ssl.SSLSocketFactory",
-                 "mail.smtp.socketFactory.fallback": "false"]
+
+environments {
+    development {
+        grails.mail.host = 'localhost'
+        def port = com.icegreen.greenmail.util.ServerSetupTest.SMTP.port
+        grails.mail.port = port
+        grails.plugin.greenmail.ports.smtp = port
+    }
+    test {
+        grails.mail.host = 'localhost'
+        grails.mail.port = com.icegreen.greenmail.util.ServerSetupTest.SMTP.port
+    }
+    production {
+        greenmail.disabled = true
+        grails {
+            mail {
+                host = "smtp.gmail.com"
+                port = 465
+                username = "youracount@gmail.com"
+                password = "yourpassword"
+                props = ["mail.smtp.auth"                  : "true",
+                         "mail.smtp.socketFactory.port"    : "465",
+                         "mail.smtp.socketFactory.class"   : "javax.net.ssl.SSLSocketFactory",
+                         "mail.smtp.socketFactory.fallback": "false"]
+            }
+        }
     }
 }
