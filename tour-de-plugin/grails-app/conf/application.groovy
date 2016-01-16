0d95a89c79962f0e13d8d8d0c6a1970b92ed02f8
Søren Berg Glasius <soeren@glasius.dk>
2016-01-16 12:25:28 +0530

    SpringSecurityCore s2-quickstart

--- tour-de-plugin/grails-app/conf/application.groovy
+++ tour-de-plugin/grails-app/conf/application.groovy
@@ -27,3 +27,30 @@ environments {
         }
     }
 }
+
+// Added by the Spring Security Core plugin:
+grails.plugin.springsecurity.userLookup.userDomainClassName = 'tour.de.plugin.User'
+grails.plugin.springsecurity.userLookup.authorityJoinClassName = 'tour.de.plugin.UserRole'
+grails.plugin.springsecurity.authority.className = 'tour.de.plugin.Role'
+grails.plugin.springsecurity.controllerAnnotations.staticRules = [
+	[pattern: '/',               access: ['permitAll']],
+	[pattern: '/error',          access: ['permitAll']],
+	[pattern: '/index',          access: ['permitAll']],
+	[pattern: '/index.gsp',      access: ['permitAll']],
+	[pattern: '/shutdown',       access: ['permitAll']],
+	[pattern: '/assets/**',      access: ['permitAll']],
+	[pattern: '/**/js/**',       access: ['permitAll']],
+	[pattern: '/**/css/**',      access: ['permitAll']],
+	[pattern: '/**/images/**',   access: ['permitAll']],
+	[pattern: '/**/favicon.ico', access: ['permitAll']]
+]
+
+grails.plugin.springsecurity.filterChain.chainMap = [
+	[pattern: '/assets/**',      filters: 'none'],
+	[pattern: '/**/js/**',       filters: 'none'],
+	[pattern: '/**/css/**',      filters: 'none'],
+	[pattern: '/**/images/**',   filters: 'none'],
+	[pattern: '/**/favicon.ico', filters: 'none'],
+	[pattern: '/**',             filters: 'JOINED_FILTERS']
+]
+
