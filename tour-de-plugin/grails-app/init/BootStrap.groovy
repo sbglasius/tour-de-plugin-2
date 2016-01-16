71a6746c4a6711428ca3f7ad3fb8cf238a048287
Søren Berg Glasius <soeren@glasius.dk>
2016-01-16 12:31:03 +0530

    SpringSecurityCore add Role+User

--- tour-de-plugin/grails-app/init/BootStrap.groovy
+++ tour-de-plugin/grails-app/init/BootStrap.groovy
@@ -1,5 +1,8 @@
 import groovy.json.JsonSlurper
 import tour.Team
+import tour.de.plugin.Role
+import tour.de.plugin.User
+import tour.de.plugin.UserRole
 
 import javax.servlet.ServletContext
 
@@ -9,6 +12,27 @@ class BootStrap {
         if (!Team.count()) {
             println servletContext.getResource('/tourData.json').file
             importTeamData(servletContext.getResourceAsStream('/tourData.json'))
+        }
+
+        if (!Role.count() && !User.count()) {
+            def adminRole = new Role('ROLE_ADMIN').save()
+            def userRole = new Role('ROLE_USER').save()
+
+            def adminUser = new User('admin', 'password').save()
+            def regularUser = new User('regular', 'password').save()
+
+            UserRole.create adminUser, adminRole
+            UserRole.create regularUser, userRole
+
+            UserRole.withSession {
+                it.flush()
+                it.clear()
+            }
+
+            assert User.count() == 2
+            assert Role.count() == 2
+            assert UserRole.count() == 2
+
         }
     }
 
